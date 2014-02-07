8f5d6e4f3c243d2540469673e67b9b1b6b9923f1
katsuhide <kamonegi.gg@gmail.com>
2014-02-08 03:14:02 +0900

    udpate

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Mikan::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'hello#view'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
