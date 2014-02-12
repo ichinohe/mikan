21e4481fd14a3bde8261037890cf4b7afa2e3553
katsuhide <kamonegi.gg@gmail.com>
2014-02-13 01:11:53 +0900

    update

--- app/assets/javascripts/makeboxes.js
+++ app/assets/javascripts/makeboxes.js
@@ -184,8 +184,8 @@ function insertHTML(size, i) {
 	// by color
 	// var e = '<div class="box size' + size +  size + '" style="background-color:' + color_arr[random_color] + ';width:300px;-moz-background-size:cover;background-size:cover;"></div>';
 	// by image
-	// var e = '<div class="box size' + size +  size + '" style="background-image:url(' + array_img[i] + ');width:300px;-moz-background-size:cover;background-size:cover;"></div>';
-	var e = '<div class="box size' + size +  size + '" style="background-image:url(' + sake_img[i] + ');width:300px;-moz-background-size:cover;background-size:cover;"></div>';
+	var e = '<div class="box size' + size +  size + '" style="background-image:url(' + array_img[i] + ');width:300px;-moz-background-size:cover;background-size:cover;"></div>';
+	// var e = '<div class="box size' + size +  size + '" style="background-image:url(' + sake_img[i] + ');width:300px;-moz-background-size:cover;background-size:cover;"></div>';
 	$( '#container' ).html(  e + $( '#container' ).html() );
 
 	setTimeout( 'timer()', 10000 );
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Mikan::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'hello#view'
+  root :to => 'hello#view'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
